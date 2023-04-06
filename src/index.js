96fd09de2604d138a226c810fb38d2ce8e24114a
Ian Scott Martin <ianscottmartin@gmail.com>
2023-04-06 17:50:43 -0600

    what

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const init = () => {
   const inputForm = document.querySelector("form");
+
   inputForm.addEventListener('submit', (event) => {
     event.preventDefault();
     // console.log(event);
@@ -9,11 +10,16 @@ const init = () => {
 
     console.log(input.value);
 
-    fetch("http://localhost:3000/movies")
+    fetch(`http://localhost:3000/movies/${input.value}`)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        // LOG: (3) [{…}, {…}, {…}]
+        const title = document.querySelector
+        ("section#movieDetails h4");
+        const summary = document.querySelector
+        ("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
       });
 
     });
@@ -22,3 +28,4 @@ const init = () => {
 
 
 document.addEventListener('DOMContentLoaded', init);
+
